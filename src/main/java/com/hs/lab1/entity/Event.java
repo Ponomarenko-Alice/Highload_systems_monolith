a8a657cdacc39a22995d3721d596634369aa3921
dazavv <dasha7348@gmail.com>
2025-09-25 19:58:45 +0300

    feat(event) [5]: добавлены модели Event, GroupEvent

--- src/main/java/com/hs/lab1/entity/Event.java
+++ src/main/java/com/hs/lab1/entity/Event.java
@@ -1,4 +1,33 @@
 package com.hs.lab1.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank()
+    @Size(max = 20)
+    private String name;
+
+    @NotBlank()
+    private LocalDate date;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
 }
--- src/main/java/com/hs/lab1/entity/GroupEvent.java
+++ src/main/java/com/hs/lab1/entity/GroupEvent.java
@@ -1,4 +1,46 @@
 package com.hs.lab1.entity;
 
+import com.hs.lab1.enums.GroupEventStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class GroupEvent {
+    @Id
+    private Long id;
+
+    @NotBlank
+    @Size(max = 30)
+    private String name;
+
+    @Size(max = 200)
+    private String description;
+
+    private LocalDate date;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_group_events",
+            joinColumns = @JoinColumn(name = "group_event_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> participants;
+
+    @Enumerated(EnumType.STRING)
+    private GroupEventStatus status;
 }
