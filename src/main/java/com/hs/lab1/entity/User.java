afd61052f0dbb53993091de3289910a0e8a68be7
dazavv <dasha7348@gmail.com>
2025-09-25 19:58:09 +0300

    feat(user) [5]: добавлена модель User

--- src/main/java/com/hs/lab1/entity/User.java
+++ src/main/java/com/hs/lab1/entity/User.java
@@ -1,4 +1,35 @@
 package com.hs.lab1.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank()
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank()
+    @Size(max = 20)
+    private String name;
+
+    @NotBlank()
+    @Size(max = 30)
+    private String surname;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Event> events;
 }
