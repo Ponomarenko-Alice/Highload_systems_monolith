a4dcb370b2c729b83f296f07ff4f979cb828cd1b
Ponomarenko Alice <a.ponomarenko111@gmail.com>
2025-09-29 12:58:27 +0300

    feat: Реализация контроллера и сервиса создания групповых задач

--- src/main/java/com/hs/lab1/controller/GroupEventController.java
+++ src/main/java/com/hs/lab1/controller/GroupEventController.java
@@ -1,7 +1,38 @@
 package com.hs.lab1.controller;
 
+import com.hs.lab1.dto.GroupEventDto;
+import com.hs.lab1.entity.GroupEvent;
+import com.hs.lab1.mapper.GroupEventMapper;
+import com.hs.lab1.mapper.UserMapper;
+import com.hs.lab1.requests.CreateGroupEventRequest;
+import com.hs.lab1.service.GroupEventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/groupEvent")
+@RequiredArgsConstructor
 public class GroupEventController {
+    private final GroupEventService groupEventService;
+    private final GroupEventMapper groupEventMapper;
+    private final UserMapper userMapper;
+
+    @PostMapping
+    public ResponseEntity<GroupEventDto> addGroupEvent(@RequestBody CreateGroupEventRequest request) {
+        GroupEvent groupEvent = groupEventService.addGroupEvent(
+                request.name(),
+                request.description(),
+                request.date(),
+                request.startTime(),
+                request.endTime(),
+                request.participantIds(),
+                request.ownerId(),
+                request.status()
+        );
+        return ResponseEntity.ok(groupEventMapper.toGroupEventDto(groupEvent));
+    }
 }
